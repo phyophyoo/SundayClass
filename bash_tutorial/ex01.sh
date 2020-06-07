7b8eb141e1ae022608366c12605622583887c619
root <root@job-roller.com>
2020-06-08 01:45:17 +0900

    file delete and update

--- bash_tutorial/ex01.sh
+++ bash_tutorial/ex01.sh
@@ -5,7 +5,7 @@ echo $SCRIPT_DIR
 
 if [ -f "$SCRIPT_DIR/input.txt" ] 
 then
-   wc -l ./input.txt >> "$SCRIPT_DIR/output.txt"    
+   wc -l < input.txt >> "$SCRIPT_DIR/output.txt"    
 else
-    echo "file not found" >> "$SCRIPT_DIR/output.txt"
+    echo "error" >> "$SCRIPT_DIR/output.txt"
 fi
