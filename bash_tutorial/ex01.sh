0081b45647bea6719b7ec05f8e5ed910b0702683
root <root@job-roller.com>
2020-06-12 00:45:52 +0900

    change file

--- bash_tutorial/ex01.sh
+++ bash_tutorial/ex01.sh
@@ -5,7 +5,7 @@ echo $SCRIPT_DIR
 
 if [ -f "$SCRIPT_DIR/input.txt" ] 
 then
-   wc -l < input.txt >> "$SCRIPT_DIR/output.txt"    
+   wc -l < $SCRIPT_DIR/input.txt >> "$SCRIPT_DIR/output.txt"    
 else
     echo "error" >> "$SCRIPT_DIR/output.txt"
 fi
